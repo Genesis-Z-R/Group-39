5d7bccba800ff6bd02496a149dbf1e3e52afcb04
Genius-official <classicgenius666@gmail.com>
2025-07-22 11:41:03 +0000

    Delete src directory

--- src/main/java/com/example/BACKEND_APIS/Model/VoteType.java
+++
@@ -1,6 +0,0 @@
-package com.example.BACKEND_APIS.Model;
-
-public enum VoteType {
-    UPVOTE,
-    DOWNVOTE
-}
--- src/main/java/com/example/BACKEND_APIS/Repository/UserRepository.java
+++
@@ -1,6 +0,0 @@
-package com.example.BACKEND_APIS.Repository;
-
-import com.example.BACKEND_APIS.Model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-public interface UserRepository  extends JpaRepository<User, Long>{
-}
